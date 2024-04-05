fdb6ce8762d463f0a5f17b4d5fe06596d5e286ca
thingamajig2 <ttasargsyan@gmail.com>
2024-04-04 20:29:18 -0700

    route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./components/home/Home";
 import { Shop } from "./features/shop/Shop";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigate, BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
         <Shop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/affiliate' element={<Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
     </div>
