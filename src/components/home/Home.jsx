40b356b56b21dd0544ef22c4431e87fd2693fd4c
Tatevik Sargsyan <tateviksargsyan@Tateviks-Laptop.local>
2024-04-26 21:12:43 -0700

    modal

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,21 +3,44 @@ import Product from "../product/Product";
 import { useSelector, useDispatch } from 'react-redux';
 import { getProducts, selectProducts } from "../../features/shop/shopSlice";
 import { useEffect } from "react";
+import Modal from 'react-modal';
+import React from 'react';
 
-const Home = ()=>{
+const Home = () => {
     const dispatch = useDispatch();
     const productData = useSelector(selectProducts);
+    const [modalIsOpen, setIsOpen] = React.useState(true);
 
     useEffect(() => {
         dispatch(getProducts());
     }, [])
     let storedArray = JSON.parse(localStorage.getItem('myArray')) || [];
 
-    return(
-        <div className = "home">
-            <div className = "productContent">
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    return (
+        <div className="home">
+            <div>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                >
+                    <button onClick={closeModal}>x</button>
+                    <div className="intro"></div>
+                </Modal>
+            </div>
+            <div className="categories">
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+            </div>
+            <div className="productContent">
                 {
-                     productData.map(item => (
+                    productData.map(item => (
                         <Product key={item.id} item={item} />
                     ))}
             </div>
